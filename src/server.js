64efe5b2e1ee0ae839664ed8bb8ed3eb353070c7
jaehyeon2650 <ind07152@naver.com>
2023-08-03 20:09:06 +0900

    2023-08-03 morgan

--- src/server.js
+++ src/server.js
@@ -1,17 +1,15 @@
 import express from "express";
+import morgan from "morgan";
 const app = express();
+const logger = morgan("dev");
 const handleHome = (req, res) => {
     return res.send("Hi");
 }
 const handleLogin = (req, res) => {
     return res.send("Login");
 }
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-}
 
-app.use(logger)
+app.use(logger);
 app.get("/", handleHome);
 app.get("/login", handleLogin);
 const handleListening = () => {
